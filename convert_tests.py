5055be05a71c2c7fddc0c8a450ce3c860550e30b
Aaliyah <akelly47@calpoly.edu>
2024-11-11 18:41:31 -0800

    pushhh

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,18 @@
+import unittest
+from convert import str_to_float
 
+
+class TestCases(unittest.TestCase):
+    # task 1
+    def test_float_str(self):
+        self.assertEqual(str_to_float("3.14"), 3.14)
+        self.assertEqual(str_to_float("0.001"), 0.001)
+        self.assertEqual(str_to_float("100"), 100.0)
+
+    def test_float_str_2(self):
+        self.assertIsNone(str_to_float("abc"))
+        self.assertIsNone(str_to_float(""))
+        self.assertIsNone(str_to_float("3.14abc"))
+
+if __name__ == "__main__":
+    unittest.main()
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,23 @@
+from convert import str_to_float
 
+# Function: Gathers numbers from the user until 'done' is entered
+# Input: No input
+# Output: List of floats that the user has entered
+def gather_numbers() -> list[float]:
+    numbers = []
+    while True:
+        user_input = input("Enter a number, type 'done' when finished: ")
+        # check for exit condition
+        if user_input.lower() == "done":
+            break
+        # try to convert to float and add to list if valid
+        number = str_to_float((user_input))
+        if number is not None:
+            numbers.append(number)
+        else:
+            print("Invalid input.")
+    return numbers
+
+if __name__ == '__main__':
+    numbers = gather_numbers()
+    print("Sum of numbers:", sum(numbers))
